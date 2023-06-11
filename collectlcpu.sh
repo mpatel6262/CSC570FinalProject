cde98396457c5b7bc91a58286c9e1e4a6f2b6574
Maanav Patel <71287606+mpatel6262@users.noreply.github.com>
2023-06-11 14:50:51 -0700

    Update collectlcpu.sh

--- collectlcpu.sh
+++ collectlcpu.sh
@@ -2,14 +2,9 @@
 
 echo "@@@INSIDE SERVER COLLECTL SCRIPT@@@"
 
-outputfile="output1.txt"
+sudo collectl -sCD >& ~/collectl/"$1"&
 
-#collectl -sC >& outputfile 
-#collectl -sC -P -f /home/joydeep/results/collectl/"$outputfile" &
-#collectl   -sC -c 1000  >> /home/joydeep/collectl/"$outputfile"&
-sudo collectl -sCD >& /home/ubuntu/engg_533/collectl/"$1"&
-#sudo collectl -sZ -i:1 --procopts m >& /home/joydeep/freshtest/collectl/lowthroughput/four_cores_diff_socket/l3cache/"$outputfile"&
-#sudo collectl -sZ -i1:1 >> /home/joydeep/lighttpd/collectl/test1/"$outputfile"&
 echo "@@@OUTSIDE SERVER COLLECTL SCRIPT@@@"
+
 sleep 1 
 #sudo killall collectl
