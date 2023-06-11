c572a06049277caede7e035d1785db0951f4393d
Maanav Patel <71287606+mpatel6262@users.noreply.github.com>
2023-06-11 14:50:00 -0700

    Update sar.sh

--- sar.sh
+++ sar.sh
@@ -2,14 +2,8 @@
 
 echo "@@@INSIDE SAR SCRIPT@@@"
 
-outputfile="output1.txt"
+sudo sar -n DEV 1 | grep eth0 >& ~/collectl/"$1"&
 
-#collectl -sC >& outputfile 
-#collectl -sC -P -f /home/joydeep/results/collectl/"$outputfile" &
-#collectl   -sC -c 1000  >> /home/joydeep/results/collectl/"$outputfile"&
-sudo sar -n DEV 1 | grep eth0 >& /home/ubuntu/engg_533/collectl/"$1"&
-#sudo collectl -sZ -i:1 --procopts m >& /home/joydeep/freshtest/collectl/lowthroughput/four_cores_diff_socket/l3cache/"$outputfile"&
-#sudo collectl -sZ -i1:1 >> /home/joydeep/lighttpd/collectl/test1/"$outputfile"&
 echo "@@@OUTSIDE SAR SCRIPT@@@"
 sleep 1 
 #sudo killall collectl
