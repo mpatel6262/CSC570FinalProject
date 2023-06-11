21e307be91d4a4444c224570f23ab80ba2628c3c
Maanav Patel <71287606+mpatel6262@users.noreply.github.com>
2023-06-11 13:04:19 -0700

    Add files via upload

---
+++ collectlcpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "@@@INSIDE SERVER COLLECTL SCRIPT@@@"
+
+outputfile="output1.txt"
+
+#collectl -sC >& outputfile 
+#collectl -sC -P -f /home/joydeep/results/collectl/"$outputfile" &
+#collectl   -sC -c 1000  >> /home/joydeep/collectl/"$outputfile"&
+sudo collectl -sCD >& /home/ubuntu/engg_533/collectl/"$1"&
+#sudo collectl -sZ -i:1 --procopts m >& /home/joydeep/freshtest/collectl/lowthroughput/four_cores_diff_socket/l3cache/"$outputfile"&
+#sudo collectl -sZ -i1:1 >> /home/joydeep/lighttpd/collectl/test1/"$outputfile"&
+echo "@@@OUTSIDE SERVER COLLECTL SCRIPT@@@"
+sleep 1 
+#sudo killall collectl
